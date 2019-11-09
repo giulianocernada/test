36508b8716c37cbe6515691459baf3cd1251068d
Giuliano Cernada <giuliano.cernada@hotmail.com>
2019-11-09 20:44:06 -0300

    Updates

---
+++ precache-manifest.3f68fb0e7513e42e85d52079d8c7528c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfb6cd920d28058ccdc0f22568c8934",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1dda4de4d88e6fc61a00",
+    "url": "/my-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "734aced0bfa9fdded10f",
+    "url": "/my-app/static/js/2.dd44167c.chunk.js"
+  },
+  {
+    "revision": "1dda4de4d88e6fc61a00",
+    "url": "/my-app/static/js/main.27310b42.chunk.js"
+  },
+  {
+    "revision": "d9eab8354aa92732f1c4",
+    "url": "/my-app/static/js/runtime-main.d036e624.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-app/static/media/logo.25bf045c.svg"
+  }
+]);
